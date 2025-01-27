72e33c2b62757558954935ab68a4831fbb5e537b
jeelpatel0362 <jeelpatel0362@gmail.com>
2025-01-27 15:34:56 +0530

    home screen

--- app/src/main/java/com/example/bankbalancecheckapp/TransferMoney.java
+++ app/src/main/java/com/example/bankbalancecheckapp/TransferMoney.java
@@ -1,6 +1,10 @@
 package com.example.bankbalancecheckapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,67 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class TransferMoney extends AppCompatActivity {
+    private EditText recipient , amount ;
+    private UserData userData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_transfer_money);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        userData = (UserData) getIntent().getSerializableExtra("user");
+
+        recipient = findViewById(R.id.accountNumber_content);
+        amount = findViewById(R.id.amount_content);
+
+        findViewById(R.id.sendMoneyBtn).setOnClickListener(this::sendMoney);
     }
+
+    private  void sendMoney(View view){
+
+
+        String recipientText = recipient.getText().toString().trim();
+        String amountText = amount.getText().toString().trim();
+
+        if(amountText.isEmpty() && recipientText.isEmpty()){
+            Toast.makeText(this,"Please fill all details",Toast.LENGTH_SHORT).show();
+            return ;
+        }
+
+        if(recipientText.isEmpty())
+        {
+            Toast.makeText(this,"Recipient account number field is Empty",Toast.LENGTH_SHORT).show();
+            return ;
+        } else if (recipientText.equals(userData.getAccountNumber())) {
+            Toast.makeText(this,"You cannot send money to your own account",Toast.LENGTH_SHORT).show();
+            return ;
+
+        }
+
+        if(amountText.isEmpty()){
+            Toast.makeText(this,"Amount field is Empty",Toast.LENGTH_SHORT).show();
+            amount.setError("Amount is Empty");
+            return ;
+        }
+
+        double amount = Double.parseDouble(amountText);
+
+        if(amount > userData.getBalance())
+        {
+            Toast.makeText(this,"Insufficient Balance",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        userData.setBalance(userData.getBalance()-amount);
+        userData.addTransaction("₹" + amount +  " sent to " + recipientText );
+
+        Toast.makeText(this,"Transaction successfull",Toast.LENGTH_SHORT).show();
+
+
+        Intent intent = new Intent(this,HomeScreen.class);
+        intent.putExtra("user",userData);
+        startActivity(intent);
+    }
+
 }
